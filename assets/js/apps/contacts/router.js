de7abd3ce9a7bcb5d24120dab8f2920f69f192c9
dezoxel <dezoxel@gmail.com>
2014-12-06 16:59:27 +0200

    Added show contact route handling

--- assets/js/apps/contacts/router.js
+++ assets/js/apps/contacts/router.js
@@ -1,19 +1,29 @@
 ContactManager.module('ContactsApp', function(ContactsApp, ContactManager, Backbone, Marionette, $, _) {
   ContactsApp.Router = Marionette.AppRouter.extend({
     appRoutes: {
-      'contacts': 'listContacts'
+      'contacts': 'listContacts',
+      'contacts/:id': 'showContact'
     }
   });
 
   var API = {
     listContacts: function() {
       ContactManager.ContactsApp.List.Controller.listContacts();
+    },
+
+    showContact: function(id) {
+      ContactManager.ContactsApp.Show.Controller.showContact(id);
     }
   };
 
   ContactManager.on('contacts:list', function() {
     ContactManager.navigateTo('contacts');
     API.listContacts();
+  });
+
+  ContactManager.on('contact:show', function(id) {
+    ContactManager.navigateTo('contacts/' + id);
+    API.showContact(id);
   });
 
   ContactManager.addInitializer(function() {
